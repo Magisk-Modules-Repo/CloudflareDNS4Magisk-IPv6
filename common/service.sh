9512d014c93d1045b37c5ed5607e1745c3365d8e
Rom <skav@gmx.fr>
2019-03-29 01:01:20 +0100

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,12 +1,15 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
-# More info in the main Magisk thread
 
-ip6tables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to destination 2606:4700:4700::1111
-ip6tables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to destination 2606:4700:4700::1001
-ip6tables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 2606:4700:4700::1111
-ip6tables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 2606:4700:4700::1001
+iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
+iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination 1.0.0.1:53
+iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
+iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 1.0.0.1:53
+
