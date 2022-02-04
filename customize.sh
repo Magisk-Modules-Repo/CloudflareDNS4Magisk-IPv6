231fabe5359e187f3153317cf1cb41fc2ab01d77
gjf <gjf.fixxxer@gmail.com>
2022-02-04 20:27:55 +0300

    MODPATH is better than TMP

--- customize.sh
+++ customize.sh
@@ -1,5 +1,5 @@
 if [ -a /system/etc/resolv.conf ]; then
-	mkdir -p $MODPATH/system/etc/
-	printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $MODPATH/system/etc/resolv.conf
-	touch $MODPATH/auto_mount
+	mkdir -p $TMPDIR/system/etc/
+	printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" >> $TMPDIR/system/etc/resolv.conf
+	touch $TMPDIR/auto_mount
 fi
